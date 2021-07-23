ba13adfb13d4786f6d415dfb4ecc477096de13fb
Jean Morrison <beatsandbeets275@gmail.com>
2021-07-23 14:29:53 -0500

    Minor update for points system

--- CapstoneProject/game.js
+++ CapstoneProject/game.js
@@ -4,7 +4,9 @@ var turn = true;
 // Wheel spins fast then more slowly and then stops
 var spinInterval = 0;
 var spinResistance = 0;
+var rotation;
 var globalPhrase;
+var points;
 
 function timeSpin() {
   var loop = setInterval(function () {
@@ -13,6 +15,7 @@ function timeSpin() {
     let rotate = `rotate(${spinInterval}deg)`;
     document.querySelector(".container").style.transform = rotate;
     // console.log(rotate);
+    rotation = spinInterval;
   }, 100);
 
   setTimeout(function () {
@@ -23,11 +26,19 @@ function timeSpin() {
   document.getElementById("submitButton").disabled = false;
 }
 
+//reads the rotation and gives points
+function clearInt(loop) {
+  clearInterval(loop);
+  assignPoints();
+}
+
 //An array of phrases
 const MARY = { phrase: "  MARY HAD A    LITTLE LAMB ", type: "Phrase" };
 const SPIN = { phrase: "  SPIN THAT       WHEEL     ", type: "Phrase" };
 const PARIS = { phrase: "    EIFFEL        TOWER     ", type: "Place" };
 const EMPIRE = { phrase: " EMPIRE STATE    BUILDING   ", type: "Place" };
+const LONDON = { phrase: "   BIG BEN      IN LONDON   ", type: "Place"};
+const PRICE = { phrase: "  THE PRICE      IS RIGHT   ", type: "Phrase"};
 
 let phrasesArray = [MARY, SPIN, PARIS, EMPIRE];
 
@@ -89,6 +100,7 @@ function getInput() {
       if ((input === phrase[i])) {
         console.log(`item${i+1}`);
         displayLetter(`item${i+1}`, phrase[i]);
+        assignPoints();
       }
     }
   } else {
@@ -96,4 +108,43 @@ function getInput() {
   }
 }
 
-
+//assigns points
+function assignPoints() {
+  let value = document.querySelector(".container").style.rotation % 360;
+  console.log(value);
+  if(0<=value<45) {
+    points += 100;
+    console.log(points);
+    document.getElementById("points").innerHTML = `Points: ${points}`;
+  } else if (45<=value<90) {
+    points += 50;
+    console.log(points);
+    document.getElementById("points").innerHTML = `Points: ${points}`;
+  } else if (90<=value<135) {
+    points += 500;
+    console.log(points);
+    document.getElementById("points").innerHTML = `Points: ${points}`;
+  } else if (135<=value<180) {
+    points = 0;
+    console.log(points);
+    document.getElementById("points").innerHTML = `Points: ${points}`;
+  } else if (180<=value<225) {
+    points += 500;
+    console.log(points);
+    document.getElementById("points").innerHTML = `Points: ${points}`;
+  } else if (225<=value<270) {
+    points += 50;
+    console.log(points);
+    document.getElementById("points").innerHTML = `Points: ${points}`;
+  } else if (270<=value<315) {
+    points += 200;
+    console.log(points);
+    document.getElementById("points").innerHTML = `Points: ${points}`;
+  } else if (315<=value<360) {
+    points += 300;
+    console.log(points);
+    document.getElementById("points").innerHTML = `Points: ${points}`;
+  } else {
+    alert(There-is-a-problem);
+  }
+}
